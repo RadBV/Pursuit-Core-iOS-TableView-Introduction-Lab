d006d94cc12dac698f6ab178d2773ced8d089559
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-08-13 13:45:26 -0400

    replaces starting view controller with a table view controller and adds table view controller/table view cell cocoa files

---
+++ TableViewIntroductionLab/TableViewIntroductionLab/ToDoListCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoListCell.swift
+//  TableViewIntroductionLab
+//
+//  Created by Radharani Ribas-Valongo on 8/13/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
