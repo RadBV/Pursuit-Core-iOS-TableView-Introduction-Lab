7a0213426a94cd83efdfb1945f7d45e5aa7fd812
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-08-13 15:32:33 -0400

    adds title/subtitle to cells and adds task name and due date to them

--- TableViewIntroductionLab/TableViewIntroductionLab/ToDoListTableViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ToDoListTableViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ToDoListTableViewController: UITableViewController {
 
+    let tasks = TaskData.allTasks
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -24,23 +25,26 @@ class ToDoListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tasks.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoListCell", for: indexPath)
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.name
+        cell.detailTextLabel?.text = task.dueDate.description
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
